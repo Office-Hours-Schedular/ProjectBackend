3256d7e418568b7cfca5dd87206956dd776dadb2
Ramaz Tskhadadze <bubu@Ramazs-MacBook-Pro-2.local>
2024-04-26 15:58:54 -0700

    feat: get student items

--- routes/schedule.js
+++ routes/schedule.js
@@ -2,17 +2,27 @@ const express = require("express");
 const router = express.Router();
 const scheduleController = require("../controllers/sheduleController.js");
 
-
 router.post("/professor/schedule/:userId", scheduleController.addProfessorItem);
 
-router.delete("/professor/schedule/:userId", scheduleController.removeProfessorItem);
+router.delete(
+  "/professor/schedule/:userId",
+  scheduleController.removeProfessorItem
+);
 
-router.delete("/professor/schedule/recurring/:userId", scheduleController.removeProfessorRecurringItem);
+router.delete(
+  "/professor/schedule/recurring/:userId",
+  scheduleController.removeProfessorRecurringItem
+);
 
-router.get("/schedule/:professorId", scheduleController.getItems);
+router.get("/schedule/:professorId", scheduleController.getProfAndStudItems);
 
 router.post("/student/schedule/:userId", scheduleController.addStudentItem);
 
-router.delete("/student/schedule/:userId", scheduleController.removeStudentItem);
+router.post("/student/schedule/:userId", scheduleController.getStudentItems);
+
+router.delete(
+  "/student/schedule/:userId",
+  scheduleController.removeStudentItem
+);
 
 module.exports = router;
